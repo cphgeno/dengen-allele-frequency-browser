005c3e54240d2cd08cc0efc23650c788e938f2a6
mauricio <mauriciomoldesquaresma@gmail.com>
2026-06-17 09:10:58 +0200

    updated footer to 2026

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   <div>
    {/* Footer */}
       <footer className="bg-gray-800 text-white py-8 text-center">
-        <p>2025 DenGen | Developed by the Department of Genomic Medicine, Rigshospitalet </p>
+        <p>2026 DenGen | Developed by MDxCORE, Rigshospitalet </p>
       </footer>
   </div>
   );
